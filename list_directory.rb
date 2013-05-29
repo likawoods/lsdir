3780b0fc2b39ee6778b46ba176179813a94fa996
Tom Stuart <stuart@soundcloud.com>
2013-05-29 20:36:13 +0200

    lsdir project

---
+++ list_directory.rb
@@ -0,0 +1,25 @@
+# Given the name of a directory:
+# 1. For each file:
+#   if it's a normal file:
+#     show its name
+#   if it's a directory:
+#     for each file in the directory:
+#       if it's a normal file:
+#         show its name
+#       if it's a directory:
+#        ...
+#
+#USAGE: ruby list_directory.rb /home/tom/code
+
+# ARGV.first is the first argument on the command line:
+# ruby list_directory.rb . -> "Listing ."
+# ruby list_directory.rb hello -> "Listing hello"
+# ruby list_directory.rb hello world -> "Listing hello"
+puts "Listing #{ARGV.first}"
+
+# Dir[directory + "/*"] gives us an ARRAY of all files 1-level deep in directory
+# puts with an ARRAY displays the array, one entry per line
+puts Dir[ARGV.first + "/*"]
+
+# true if filename is a directory, false if not
+File.directory?(filename)
